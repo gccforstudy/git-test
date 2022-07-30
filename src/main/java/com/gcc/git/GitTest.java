5d122cb5d1d3aa45c6b51f2e87b93dff132433b8
gcc <961113371@qq.com>
2022-07-30 19:31:04 +0800

    first commit

---
+++ src/main/java/com/gcc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gcc.git;
+
+/**
+ * @author gcc
+ * @create 2022 - 07 - 30 - 16:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, git!");
+    }
+}
